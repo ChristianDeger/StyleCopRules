e4a2043101be8af487911d6294886c4d16c6dd51
Christian Deger <christian@deger.eu>
2010-02-15 17:17:20 +0100

    Role for private fields with underscore

---
+++ src/StyleCopRules/OwnRules.cs
@@ -0,0 +1,87 @@
+﻿namespace StyleCopRules
+{
+    using System;
+    using Microsoft.StyleCop;
+    using Microsoft.StyleCop.CSharp;
+
+    [SourceAnalyzer(typeof(CsParser))]
+    public class OwnRules : SourceAnalyzer
+    {
+        public override void AnalyzeDocument(CodeDocument document)
+        {
+            Param.RequireNotNull(document, "document");
+            var csDocument = (CsDocument)document;
+            if ((csDocument.RootElement != null) && !csDocument.RootElement.Generated)
+            {
+                csDocument.WalkDocument(VisitElement);
+            }
+        }
+
+        private bool VisitElement(CsElement element, CsElement parentelement, object context)
+        {
+            if (element.ElementType == ElementType.Field)
+            {
+                VisitField((Field)element);
+            }
+
+            return true;
+        }
+
+        private void VisitField(Field field)
+        {
+            PrivateFieldNameMustStartWithUnderscoreFollowedByLowerCase(field);
+        }
+
+        private void PrivateFieldNameMustStartWithUnderscoreFollowedByLowerCase(Field field)
+        {
+            if (field.Generated)
+            {
+                return;
+            }
+
+            if (field.Const)
+            {
+                return;
+            }
+
+            if (IsReadonlyStatic(field))
+            {
+                return;
+            }
+
+            if (ViolatesFieldNameRule(field.Declaration.Name))
+            {
+                AddViolation(
+                    field,
+                    "PrivateFieldNameMustStartWithUnderscoreFollowedByLowerCase",
+                    new object[0]);
+            }
+        }
+
+        private static bool ViolatesFieldNameRule(string fieldName)
+        {
+            if (!fieldName.StartsWith("_"))
+            {
+                return true;
+            }
+
+            return SecondCharNotLowercase(fieldName);
+        }
+
+        private static bool SecondCharNotLowercase(string fieldName)
+        {
+            if (fieldName.Length < 2)
+            {
+                return false;
+            }
+
+            var secondChar = fieldName.Substring(1, 1);
+            return !Char.IsLower(secondChar[0]);
+        }
+
+        private static bool IsReadonlyStatic(Field field)
+        {
+            return field.Readonly && field.Declaration.ContainsModifier(new[] { CsTokenType.Static });
+        }
+    }
+}
---
+++ src/StyleCopRules/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StyleCopRules")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CD")]
+[assembly: AssemblyProduct("StyleCopRules")]
+[assembly: AssemblyCopyright("Copyright © Christian Deger 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("469fb7d6-8256-42b0-87d0-1908a4eefa53")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
